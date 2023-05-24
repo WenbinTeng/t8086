c613437bc9f477db2b4315fd8cb11e8a099d61e6
WenbinTeng <WenbinTeng@163.com>
2023-05-24 16:20:39 +0800

    upload top module

--- src/top.v
+++ src/top.v
@@ -19,5 +19,63 @@ module top (
     input           hold,   // hold request
     output          hlda    // hold acknowledge
 );
-    
+    wire            rom_en;
+    wire    [15:0]  rom_addr;
+    wire    [ 7:0]  rom_data;
+
+    wire            ram_rd_en;
+    wire            ram_rd_we;
+    wire            ram_rd_de;
+    wire    [15:0]  ram_rd_addr;
+    wire    [ 7:0]  ram_rd_data;
+
+    wire            ram_wr_en;
+    wire            ram_wr_we;
+    wire            ram_wr_de;
+    wire    [15:0]  ram_wr_addr;
+    wire    [ 7:0]  ram_wr_data;
+
+    core u_core (
+        .clk            (clk),
+        .rst            (rst),
+        
+        .rom_en         (rom_en),
+        .rom_addr       (rom_addr),
+        .rom_data       (rom_data),
+
+        .ram_rd_en      (ram_rd_en),
+        .ram_rd_we      (ram_rd_we),
+        .ram_rd_de      (ram_rd_de),
+        .ram_rd_addr    (ram_rd_addr),
+        .ram_rd_data    (ram_rd_data),
+
+        .ram_wr_en      (ram_wr_en),
+        .ram_wr_we      (ram_wr_we),
+        .ram_wr_de      (ram_wr_de),
+        .ram_wr_addr    (ram_wr_addr),
+        .ram_wr_data    (ram_wr_data)
+    );
+
+    ram u_ram (
+        .clk            (clk),
+
+        .ram_rd_en      (ram_rd_en),
+        .ram_rd_we      (ram_rd_we),
+        .ram_rd_de      (ram_rd_de),
+        .ram_rd_addr    (ram_rd_addr),
+        .ram_rd_data    (ram_rd_data),
+
+        .ram_wr_en      (ram_wr_en),
+        .ram_wr_we      (ram_wr_we),
+        .ram_wr_de      (ram_wr_de),
+        .ram_wr_addr    (ram_wr_addr),
+        .ram_wr_data    (ram_wr_data)
+    );
+
+    rom u_rom (
+        .rom_en         (rom_en),
+        .rom_addr       (rom_addr),
+        .rom_data       (rom_data)
+    );
+
 endmodule
